c98def57f3763e11f31eeeb8375ef768f5d1da7c
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2018-07-18 16:40:52 +0200

    Add help description for custom fields

--- connector_infor_account_move/models/infor_account_journal_custom_field.py
+++ connector_infor_account_move/models/infor_account_journal_custom_field.py
@@ -16,9 +16,22 @@ class InforBackendJournalCustomField(models.Model):
     data_type = fields.Selection(
         [('static', 'Static'), ('dynamic', 'Dynamic')],
     )
-    field_value = fields.Char(string='Value')
-    field = fields.Char()
-    field_default_value = fields.Char(string='Default Value')
+    field_value = fields.Char(
+        string='Value',
+        help='Used for static type fields',
+    )
+    field = fields.Char(
+        string='Dynamic Field',
+        help='Dynamicaly computed in a record fields hierarchy. If starting '
+             'by "object." the account.move.line will be used as base. If '
+             'starting by "backend." then the infor.backend is used.'
+             'If the field can not be resolved the value in "default_value" will '
+             'be used instead.',
+    )
+    field_default_value = fields.Char(
+        string='Default Value',
+        help='Used if the dynamic field resolution fails.',
+    )
     backend_id = fields.Many2one(
         comodel_name='infor.backend',
         string='Infor Backend',
